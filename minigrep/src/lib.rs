3d1d8f73749c4922dda340cd92cb6b4847308adb
NerdyPepper <akshayoppiliappan@gmail.com>
2018-06-13 16:53:15 +0530

    Update minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -3,87 +3,88 @@ use std::fs::File;
 use std::error::Error;
 use std::env;
 
+// this is the comment
 pub struct Config{
-	pub query: String,
-	pub filename: String,
-	pub case_sensitive: bool,
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
-	pub fn new(argvec: &[String]) -> Result<Config, &'static str> {
-		if argvec.len() < 3 {
-			return Err("not enough args");
-		}
+    pub fn new(argvec: &[String]) -> Result<Config, &'static str> {
+        if argvec.len() < 3 {
+            return Err("not enough args");
+        }
 
-		let query = argvec[1].clone();
-		let filename = argvec[2].clone();
-		let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        let query = argvec[1].clone();
+        let filename = argvec[2].clone();
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-		Ok( Config {
-			query,
-			filename,
-			case_sensitive,
-		} )
-	}
+        Ok( Config {
+            query,
+            filename,
+            case_sensitive,
+        } )
+    }
 }
 
 pub fn run(config: Config) -> Result<(), Box<Error>> {
-	let mut f = File::open(config.filename)?;
+    let mut f = File::open(config.filename)?;
 
-	let mut filecontents = String::new();
-	f.read_to_string(&mut filecontents)?;
+    let mut filecontents = String::new();
+    f.read_to_string(&mut filecontents)?;
 
-	let results = if config.case_sensitive {
-		search(&config.query, &filecontents)
-	} else {
-		search_case_insensitive(&config.query, &filecontents)
-	};
+    let results = if config.case_sensitive {
+        search(&config.query, &filecontents)
+    } else {
+        search_case_insensitive(&config.query, &filecontents)
+    };
 
-	for item in results {
-		println!("{}", item);
-	}
+    for item in results {
+        println!("{}", item);
+    }
 
-	Ok(())
+    Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	let mut results = Vec::new();
+    let mut results = Vec::new();
 
-	for line in contents.lines() {
-		if line.contains(query) {
-			results.push(line);
-		}
-	}
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
 
-	results
+    results
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	let mut results = Vec::new();
-	let query = query.to_lowercase();
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
 
-	for line in contents.lines() {
-		if line.to_lowercase().contains(&query) {
-			results.push(line);
-		}
-	}
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
 
-	results
+    results
 }
 
 mod test {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn first_search() {
-		let query = "iS";
-		let contents = "\
-		This is just
-		fine";
+    #[test]
+    fn first_search() {
+        let query = "iS";
+        let contents = "\
+        This is just
+        fine";
 
-		assert_eq!(
-			vec!["This is just"],
-			search_case_insensitive(query, contents)
-			);
-	}
+        assert_eq!(
+            vec!["This is just"],
+            search_case_insensitive(query, contents)
+            );
+    }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,15 +6,15 @@ use std::process;
 use minigrep::*;
 
 fn main() {
-	let argvec: Vec<String> = env::args().collect();
+    let argvec: Vec<String> = env::args().collect();
 
-	let config = Config::new(&argvec).unwrap_or_else(|err| {
-		eprintln!("Problem parsing args: {}", err);
-		process::exit(1);
-	});
+    let config = Config::new(&argvec).unwrap_or_else(|err| {
+        eprintln!("Problem parsing args: {}", err);
+        process::exit(1);
+    });
 
-	match run(config) {
-		Ok(()) => (),
-		Err(e) => eprintln!("Recieved error: {}", e)
-	};
+    match run(config) {
+        Ok(()) => (),
+        Err(e) => eprintln!("Recieved error: {}", e)
+    };
 }
